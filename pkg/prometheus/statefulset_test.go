d619a7257be450c7010060da76f62e4fbf2d653f
fatsheep9146 <zhaoziqi9146@gmail.com>
2017-12-04 13:25:55 +0800

    Add support for prometheus, alertmanager to be hostNetworked

--- pkg/prometheus/statefulset_test.go
+++ pkg/prometheus/statefulset_test.go
@@ -469,3 +469,28 @@ func TestRetention(t *testing.T) {
 		}
 	}
 }
+
+func TestHostNetwork(t *testing.T) {
+	labels := map[string]string{
+		"testlabel": "testlabelvalue",
+	}
+	annotations := map[string]string{
+		"testannotation": "testannotationvalue",
+	}
+
+	sset, err := makeStatefulSet(monitoringv1.Prometheus{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      labels,
+			Annotations: annotations,
+		},
+		Spec: monitoringv1.PrometheusSpec{
+			HostNetwork: true,
+		},
+	}, "", defaultTestConfig, nil, "")
+
+	require.NoError(t, err)
+
+	if sset.Spec.Template.Spec.HostNetwork != true || sset.Spec.Template.Spec.DNSPolicy != v1.DNSClusterFirstWithHostNet {
+		t.Fatal("HostNetwork is not set correctlly for prometheus statefulset")
+	}
+}
