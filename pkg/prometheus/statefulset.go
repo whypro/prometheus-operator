0f68e9f421145281cc32eccc4fd178cfdd08b630
Haoyu Wang <wanghaoyu@qiniu.com>
2018-09-27 17:29:55 +0000

    Update prometheus-config-reloader memory limits.

--- pkg/prometheus/statefulset.go
+++ pkg/prometheus/statefulset.go
@@ -609,7 +609,7 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *Config, ruleConfigMapName
 			Resources: v1.ResourceRequirements{
 				Limits: v1.ResourceList{
 					v1.ResourceCPU:    resource.MustParse("25m"),
-					v1.ResourceMemory: resource.MustParse("10Mi"),
+					v1.ResourceMemory: resource.MustParse("50Mi"),
 				},
 			},
 		}
@@ -842,7 +842,7 @@ func makeStatefulSetSpec(p monitoringv1.Prometheus, c *Config, ruleConfigMapName
 						Resources: v1.ResourceRequirements{
 							Limits: v1.ResourceList{
 								v1.ResourceCPU:    resource.MustParse("50m"),
-								v1.ResourceMemory: resource.MustParse("50Mi"),
+								v1.ResourceMemory: resource.MustParse("100Mi"),
 							},
 						},
 					},
